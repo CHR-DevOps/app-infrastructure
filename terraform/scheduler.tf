ad966def6b5da94054ce2290c3d2b12f2f234dd8
robertchriss <chriss.ercau@gmail.com>
2026-03-23 18:00:55 +0200

    fix scheduler

--- terraform/scheduler.tf
+++ terraform/scheduler.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "scheduler_ec2_role" {
-  name = "scheduler-ec2-start-stop-role"
+  name = "scheduler-ec2-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -16,7 +16,7 @@ resource "aws_iam_role" "scheduler_ec2_role" {
 }
 
 resource "aws_iam_role_policy" "scheduler_ec2_policy" {
-  name = "scheduler-ec2-start-stop-policy"
+  name = "scheduler-ec2-policy"
   role = aws_iam_role.scheduler_ec2_role.id
 
   policy = jsonencode({
@@ -28,50 +28,50 @@ resource "aws_iam_role_policy" "scheduler_ec2_policy" {
           "ec2:StartInstances",
           "ec2:StopInstances"
         ]
-        Resource = aws_instance.k8s_main.arn
+        Resource = aws_instance.k8s_devstaging.arn
       }
     ]
   })
 }
 
 resource "aws_scheduler_schedule" "start_ec2_morning" {
-  name       = "start-k8s-main-morning"
-  group_name = "default"
+  name                         = "start-k8s-dev-staging-morning"
+  group_name                   = "default"
+  schedule_expression          = "cron(0 6 ? * MON-FRI *)"
+  schedule_expression_timezone = "Europe/Bucharest"
+  state                        = "ENABLED"
 
   flexible_time_window {
     mode = "OFF"
   }
-
-  schedule_expression          = "cron(0 8 * * ? *)"
-  schedule_expression_timezone = "Europe/Bucharest"
 
   target {
     arn      = "arn:aws:scheduler:::aws-sdk:ec2:startInstances"
     role_arn = aws_iam_role.scheduler_ec2_role.arn
 
     input = jsonencode({
-      InstanceIds = [aws_instance.k8s_main.id]
+      InstanceIds = [aws_instance.k8s_devstaging.id]
     })
   }
 }
 
 resource "aws_scheduler_schedule" "stop_ec2_night" {
-  name       = "stop-k8s-main-night"
-  group_name = "default"
+  name                         = "stop-k8s-dev-staging-night"
+  group_name                   = "default"
+  schedule_expression          = "cron(0 22 ? * MON-FRI *)"
+  schedule_expression_timezone = "Europe/Bucharest"
+  state                        = "ENABLED"
 
   flexible_time_window {
     mode = "OFF"
   }
-
-  schedule_expression          = "cron(0 23 * * ? *)"
-  schedule_expression_timezone = "Europe/Bucharest"
 
   target {
     arn      = "arn:aws:scheduler:::aws-sdk:ec2:stopInstances"
     role_arn = aws_iam_role.scheduler_ec2_role.arn
 
     input = jsonencode({
-      InstanceIds = [aws_instance.k8s_main.id]
+      InstanceIds = [aws_instance.k8s_devstaging.id]
     })
   }
 }
